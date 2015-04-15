5e0be56ed36e5852e06109cb5b6e54ca58f75391
Thrasi <thrasi@server.fake>
2015-04-15 14:03:21 +0200

    baracs

--- T102/Barracks.java
+++ T102/Barracks.java
@@ -1,12 +1,20 @@
 package T102;
 
+import battlecode.common.GameActionException;
 import battlecode.common.RobotController;
+import battlecode.common.RobotType;
 
 public class Barracks extends BaseBot {
 
 	public Barracks(RobotController rc) {
 		super(rc);
 		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public void execute() throws GameActionException {
+		trySpawn(RobotType.SOLDIER);
+		rc.yield();
 	}
 
 }
