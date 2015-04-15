dc5bd0722572bbc7377f5e19b8416ce9309c7056
dario-wat <dario.vidas92@gmail.com>
2015-04-15 13:59:34 +0200

    Helipad started

--- T102/Helipad.java
+++ T102/Helipad.java
@@ -1,12 +1,19 @@
 package T102;
 
+import battlecode.common.GameActionException;
 import battlecode.common.RobotController;
+import battlecode.common.RobotType;
 
 public class Helipad extends BaseBot {
 
 	public Helipad(RobotController rc) {
 		super(rc);
-		// TODO Auto-generated constructor stub
 	}
 
+	@Override
+	public void execute() throws GameActionException {
+		if (rc.readBroadcast(RobotPlayer.numDRONE) < 4) {
+			trySpawn(RobotType.DRONE);
+		}
+	}
 }
